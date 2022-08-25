b2844c3987080aaaf34446877a507f4a28e522eb
evkbhzg <evkbhzg@MTS-BSE-APE-151.na.corp.mckesson.com>
2022-08-25 10:43:39 +0530

    fcommit

---
+++ Demotest/src/test/Sum.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=50;
+		int b=60;
+		System.out.println("Hello world");
+
+	}
+
+}
